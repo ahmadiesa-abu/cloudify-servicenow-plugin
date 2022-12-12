9aace1edacdfda90a7b05eeb9c673724be6b6d65
Ahmad Musa <ahmad@cloudify.co>
2022-12-12 21:22:18 +0200

    working-progress

--- cloudify_snow/tasks.py
+++ cloudify_snow/tasks.py
@@ -51,14 +51,67 @@ def list_request_items(ctx, snow_host, snow_auth, **kwargs):
 @with_auth
 def create_ci(ctx, snow_host, snow_auth, **kwargs):
     table_name = ''
-    ctx.logger.info('node_type : {0}'.format(ctx.node.type))
-    if ctx.node.type == 'cloudify.nodes.snow.linux_server':
+    ctx.logger.debug('node_type : {0}'.format(ctx.node.type))
+    if ctx.node.type == 'cloudify.nodes.snow.LinuxServer':
         table_name = 'cmdb_ci_linux_server'
-    elif ctx.node.type == 'cloudify.nodes.snow.unix_server':
+    elif ctx.node.type == 'cloudify.nodes.snow.UnixServer':
         table_name = 'cmdb_ci_unix_server'
-    elif ctx.node.type == 'cloudify.nodes.snow.windows_server':
+    elif ctx.node.type == 'cloudify.nodes.snow.WindowsServer':
         table_name = 'cmdb_ci_win_server'
-    elif ctx.node.type == 'cloudify.nodes.snow.server':
+    elif ctx.node.type == 'cloudify.nodes.snow.Server':
+        table_name = 'cmdb_ci_server'
+    url = TABLE_API_PATTERN.format(host=snow_host, table_name=table_name)
+    method = "POST"
+    headers = {"Content-Type":"application/json", "Accept":"application/json"}
+    # payload = {
+    #     'cpu_count': '1',
+    #     'dns_domain': 'domain.com',
+    #     'install_status': 'Installed',
+    #     'ip_address': '1.1.1.1',
+    #     'manufacturer': 'Virtual',
+    #     'model_number': 'Virtual',
+    #     'name': 'some-server',
+    #     'ram': '2048',
+    #     'serial_number': 'Virtual',
+    #     'short_description': 'some description for {0}'.format(table_name)
+    # }
+    payload = ctx.node.properties.get('resource_config', {})
+    req = Request(method,
+                  url,
+                  json=payload,
+                  headers=headers,
+                  auth=snow_auth)
+
+    prepped = req.prepare()
+    session = Session()
+
+    try:
+        response = session.send(prepped)
+    except (RequestException, ConnectionError, HTTPError) as e:
+        raise NonRecoverableError('Exception raised: {0}'.format(str(e)))
+
+    result = {
+        'status_code': response.status_code,
+        'body': response.request.body,
+        'content': response.content
+    }
+
+    if not response.ok:
+        raise NonRecoverableError('Request failed: {0}'.format(result))
+    ctx.logger.info('Request OK: {0}'.format(result))
+
+
+@with_auth
+def delete_ci(ctx, snow_host, snow_auth, **kwargs):
+    table_name = ''
+    ctx.logger.debug('node_type : {0}'.format(ctx.node.type))
+    if ctx.node.type == 'cloudify.nodes.snow.LinuxServer':
+        table_name = 'cmdb_ci_linux_server'
+    elif ctx.node.type == 'cloudify.nodes.snow.UnixServer':
+        table_name = 'cmdb_ci_unix_server'
+    elif ctx.node.type == 'cloudify.nodes.snow.WindowsServer':
+        table_name = 'cmdb_ci_win_server'
+    elif ctx.node.type == 'cloudify.nodes.snow.Server':
         table_name = 'cmdb_ci_server'
     url = TABLE_API_PATTERN.format(host=snow_host, table_name=table_name)
     method = "POST"
